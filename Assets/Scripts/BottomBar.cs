3db5a7f94e1ce85d362e21a9859715d48cac08e6
Gonzalo Sastre <gonmtl@gmail.com>
2020-08-19 22:54:22 -0300

    Added animation to gift panel

--- Assets/Scripts/BottomBar.cs
+++ Assets/Scripts/BottomBar.cs
@@ -6,12 +6,17 @@ public class BottomBar : MonoBehaviour
 {
     public GameObject giftsPanel;
 
+
     public void OpenGifts()
     {
         if (giftsPanel != null)
         {
-            bool isActive = giftsPanel.activeSelf;
-            giftsPanel.SetActive(!isActive);
+            Animator animator = giftsPanel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+                animator.SetBool("open", !isOpen);
+            }
         }
     }
 }
