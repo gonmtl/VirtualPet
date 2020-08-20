42c96e706e0f9bfad3cd40f1b4a708d297c2dee7
Gonzalo Sastre <gonmtl@gmail.com>
2020-08-19 22:23:01 -0300

    Added scrollview on button click

---
+++ Assets/Scripts/BottomBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomBar : MonoBehaviour
+{
+    public GameObject giftsPanel;
+
+    public void OpenGifts()
+    {
+        if (giftsPanel != null)
+        {
+            bool isActive = giftsPanel.activeSelf;
+            giftsPanel.SetActive(!isActive);
+        }
+    }
+}
