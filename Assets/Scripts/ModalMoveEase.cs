201c206bfb5a7e1e538b5af0ea1191cc914a1a4b
Gonzalo Sastre <gonmtl@gmail.com>
2020-08-20 03:23:29 -0300

    Added leantween but panels not working

---
+++ Assets/Scripts/ModalMoveEase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class ModalMoveEase : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    public void moveModal()
+    {
+        if (isOpen == false)
+        {
+            moveModalIn();
+        }
+        else
+        {
+            moveModalOut();
+        }
+    }
+
+    public void moveModalIn()
+    {
+        LeanTween.moveY(gameObject, 400f, 0.6f).setEaseOutSine();
+        isOpen = true;
+    }
+    public void moveModalOut()
+    {
+        LeanTween.moveY(gameObject, -400f, 0.3f).setEaseInSine();
+        isOpen = false;
+    }
+}
